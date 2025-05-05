291b94e57cc8c186f7b9ffb73cbd313fe9be83e5
elda27 <kaz.birdstick@gmail.com>
2025-05-05 21:53:22 +0900

    add mcp server

--- mcp_table_editor/mcp/handler_tool.py
+++ mcp_table_editor/mcp/handler_tool.py
@@ -1,20 +1,25 @@
 from typing import Any, Sequence
 
 from mcp.types import TextContent, Tool
-from pydantic import BaseModel, create_model
+from pydantic import BaseModel, Field, create_model
 
-from mcp_table_editor.handler.base import BaseHandler
+from mcp_table_editor.editor import Editor
+from mcp_table_editor.handler._base_handler import BaseHandler
 
 
 class HandlerTool:
     def __init__(
         self,
         handler: type[BaseHandler[BaseModel, BaseModel]],
-        input_args: dict[str, Any] = {},
     ) -> None:
         self.handler = handler
-        self.input_args = input_args
-        self.input_schema = create_model(handler.input_schema)
+
+    @property
+    def name(self) -> str:
+        """
+        Get the name of the handler.
+        """
+        return self.handler.name
 
     def get_mcp_tool(self) -> Tool:
         """
@@ -23,13 +28,13 @@ class HandlerTool:
         return Tool(
             name=self.handler.name,
             description=self.handler.description,
-            input_schema=self.handler.input_schema.model_json_schema(),
+            inputSchema=self.handler.input_schema.model_json_schema(),
         )
 
-    def run(self, args: dict[str, Any]) -> Sequence[TextContent]:
+    def run(self, editor: Editor, args: dict[str, Any]) -> Sequence[TextContent]:
         """
         Run the tool with the given input arguments.
         """
-        handler_instance = self.handler(**self.input_args, **args)
-        response = handler_instance.handle(self.input_args)
+        handler_instance = self.handler(editor)
+        response = handler_instance.handle(**args)
         return [TextContent(type="text", text=response.model_dump_json(indent=2))]
